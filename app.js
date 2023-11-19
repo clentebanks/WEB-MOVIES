5faa84b024e3a8ce4a236f91c3a885ca7a3ac55b
clentebanks <clentebanks0@gmail.com>
2023-11-19 01:58:01 -0600

    rr

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cargarPeliculas = async () =>{
-const resultado = await fetch('https://api.themoviedb.org/3/movie/550?api_key057588601dc278a155c7e694df690864')
+const resultado = await fetch('https://api.themoviedb.org/3/movie/11?api_key=057588601dc278a155c7e694df690864')
 
 
 console.log(resultado);
